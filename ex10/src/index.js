e974e962b639259a68997c3ee97dc19548858e50
Emir Selimovic <emirselimovic8@gmail.com>
2021-09-17 09:09:14 -0700

    Travnik

---
+++ ex10/src/index.js
@@ -0,0 +1,17 @@
+function main() {
+
+
+    var myFirstName = "Emir";
+    var myLastName = "Selimovic";
+    var myHomeTown = "Sarajevovo";
+
+    return {
+        
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
